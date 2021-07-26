f4d2f3e46bd472f44d1dd5091c95a0b8bdb9741a
nanjizal <nanjizal@users.noreply.github.com>
2021-07-26 08:24:54 +0100

    Create Float32Array.hx

---
+++ src/cornerContour/io/Float32Array.hx
@@ -0,0 +1,13 @@
+/**
+ * switches between Lime, NME and haxe.io.Float32Array
+ */
+#if (lime&&!js)
+typedef Float32Array = lime.utils.Float32Array;
+#elseif ( nme ) 
+// && !dsHelperDoc )
+typedef Float32Array = nme.utils.Float32Array;
+#elseif ( gluon )
+typedef Float32Array = typedarray.Float32Array; // gluon
+#else
+typedef Float32Array = haxe.io.Float32Array; // js.lib.Float32Array
+#end
