9ef9591c86d5b3150335408d2240452cbfab0cfa
nanjizal <nanjizal@users.noreply.github.com>
2021-08-12 05:11:45 +0100

    add size to the interface.

--- src/cornerContour/IPen.hx
+++ src/cornerContour/IPen.hx
@@ -6,4 +6,5 @@ interface IPen {
                                   , cx: Float, cy: Float
                                   , ?color: Null<Int> ): Int;
     public var pos( get, set ): Float;
+    public var size( get, never ): Int;
 }
