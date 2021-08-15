fd0bed49d36cab29b1c310813ba8c0edc49fc110
nanjizal <nanjizal@users.noreply.github.com>
2021-08-15 06:32:03 +0100

    Create Quad2D.hx

---
+++ src/cornerContour/shape/structs/Quad2D.hx
@@ -0,0 +1,15 @@
+package cornerContour.shape.structs;
+import cornerContour.shape.structs.XY;
+@:structInit
+class Quad2D {
+    public var a: XY;
+    public var b: XY;
+    public var c: XY;
+    public var d: XY;
+    function new( a: XY, b: XY, c: XY, d: XY ){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+    }
+}
