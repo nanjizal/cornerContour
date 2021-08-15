9862c71217bdb7729d3e3cb9b466f4b67c775d5d
nanjizal <nanjizal@users.noreply.github.com>
2021-08-15 05:29:43 +0100

    add bare bones rectangle support

--- src/cornerContour/shape/Rect.hx
+++ src/cornerContour/shape/Rect.hx
@@ -1,1 +1,19 @@
+package cornerContour.shape;
 
+inline
+function add2DQuad( pen: IPen
+                  , ax: Float, ay: Float
+                  , bx: Float, by: Float
+                  , cx: Float, cy: Float
+                  , dx: Float, dy: Float
+                  , color: Color ): Int {
+    pen.add2DTriangle( ax, ay, bx, by, dx, dy, color );
+    pen.add2DTriangle( bx, by, cx, cy, dx, dy, color );
+    return 2;
+}
+inline
+function rectangle( pen: IPen
+                  , x: Float, y: Float
+                  , w: Float, h: Float ): Int {
+    return pen.add2DQuad( x, y, x + w, y, x + w, y + h, x, y + h, color );
+}
