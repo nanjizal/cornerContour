ad85afea608d1e77e560c259123e5d5977ad98b9
nanjizal <nanjizal@users.noreply.github.com>
2021-08-15 06:33:19 +0100

    Create XY.hx

---
+++ src/cornerContour/shape/structs/XY.hx
@@ -0,0 +1,18 @@
+
+package cornerContour.shape.structs;
+/** 
+   { x, y }
+**/
+@:structInit
+class XY {
+    public var x = 0.; 
+    public var y = 0.;
+    @:optional public var w = 1.;
+    public function new( x: Float, y: Float ){
+        this.x = x; 
+        this.y = y;
+    }
+    public function clone(): XY {
+        return { x: x, y: y };
+    }
+}
