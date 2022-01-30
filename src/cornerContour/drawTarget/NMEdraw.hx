154459df57fa5f3002a49ac63b91bf58c44d256d
nanjizal <none>
2022-01-30 17:35:16 +0000

    fixes for nme

--- src/cornerContour/drawTarget/NMEdraw.hx
+++ src/cornerContour/drawTarget/NMEdraw.hx
@@ -1,9 +1,9 @@
 package cornerContour.drawTarget;
 import cornerContour.Pen2D;
+import flash.Lib;
 
 inline
 function rearrangeDrawData( pen: Pen2D, g: nme.display.Graphics ){
-    var pen = pen2D;
     var data = pen.arr;
     var n = Std.int( data.size/7 );
     if( n == 0 ) return;
@@ -11,18 +11,18 @@ function rearrangeDrawData( pen: Pen2D, g: nme.display.Graphics ){
     var cols = new Array<Int>();
     verts[ 6 * n - 1 ] = 0.0;
     cols[ n * 3 - 1 ]  = 0;
-    var i = 0;
+    var j = 0;
     var c = 0;
     var col: Int;
     for( i in 0...n ){
        pen.pos = i;
-       verts[ i++ ] = data.ax;
-       verts[ i++ ] = data.ay;
-       verts[ i++ ] = data.bx;
-       verts[ i++ ] = data.by;
-       verts[ i++ ] = data.cx;
-       verts[ i++ ] = data.cy;
-       col = Std.int( data.color )
+       verts[ j++ ] = data.ax * 2;
+       verts[ j++ ] = data.ay * 2;
+       verts[ j++ ] = data.bx * 2;
+       verts[ j++ ] = data.by * 2;
+       verts[ j++ ] = data.cx * 2;
+       verts[ j++ ] = data.cy * 2;
+       col = Std.int( data.color );
        cols[ c++ ] = col;
        cols[ c++ ] = col;
        cols[ c++ ] = col;
@@ -30,12 +30,14 @@ function rearrangeDrawData( pen: Pen2D, g: nme.display.Graphics ){
     g.drawTriangles( verts, null, null, null, cols );
 }
 
-
+/*
+not working
 inline
 function resizeStage( view: nme.display.Sprite, e: nme.events.Event ){
     var s = Lib.current.stage;
-    var scale =  Math.min( s.stageWidth, s.stageHeight )/2.5;
+    var scale =  Math.min( s.stageWidth, s.stageHeight )*2.5;
     view.scaleX = view.scaleY = scale;
     view.x = s.stageWidth/2 - view.width/1.9;
     view.y = scale * 0.005;
 }
+*/
